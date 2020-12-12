143fb704c099fdcce4160502e3b31234458093ba
ScotSutherland <ssutherland@ucdavis.edu>
2020-12-11 21:34:59 -0800

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -15,14 +15,20 @@ String.prototype.reverse = function() {
 
 // Defines a Phrase object.
 function Phrase(content) {
+	
   this.content = content;
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
   }
 
-  // Returns true if the phrase is a palindrome, false otherwise.
+  // Returns the letters in the content.
+  this.letters = function letters() {
+		return (this.content.match(/[a-z]/gi) || []).join("");
+  }
+	
+	// Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
     return this.processedContent() === this.processedContent().reverse();
   }
